65b9ce287ec451ac76bbe455a8dd86e12c0036c1
Nepal Singh <nepalsss007@gmail.com>
2023-10-29 17:07:06 +0530

    connected successfully

--- src/app.js
+++ src/app.js
@@ -13,19 +13,33 @@ app.use(express.static(path.join(__dirname, "/../public")));
 
 
 // db connection 
-const uri = "mongodb+srv://Ignite_n:I2SbetFH0PeusgdI@cluster0.wnh37zr.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
-function connect(){
-    try{
-       mongoose.connect(uri)  
-       connectionStatus = true;
-       console.log("success")
-    }
-    catch(err){
-        console.error(err)
-    }
-}
 
-connect()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Ignite_n:I2SbetFH0PeusgdI@cluster0.7ahbezy.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.render("index.hbs");
