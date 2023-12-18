1764e89f96d47ea8161f3c6a0a2f65c8571c0e20
Nepal Singh <nepalsss007@gmail.com>
2023-12-18 20:21:08 +0530

    added mobile view navbar

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
-const list = document.querySelectorAll(".nav__item");
-list.forEach((item) => {
-  item.addEventListener("click", () => {
-    list.forEach((item) => item.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
+let list = document.getElementsByClassName("navLink-tab");
+const OnClickNav = (i)=>{
+  for(let j = 0 ; j < list.length ; j++) {
+    list[j].classList.remove("active"); 
+  }
+  list[i].classList.add("active"); 
+}
