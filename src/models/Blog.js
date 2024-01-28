3fa2eb4ee9cb66200bc1c194df5a679891b9bda9
Nepal Singh <nepalsss007@gmail.com>
2024-01-28 09:43:42 +0530

    updated Blog page

--- src/models/Blog.js
+++ src/models/Blog.js
@@ -14,8 +14,14 @@ const blogSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  imagePath: {
+    type: String,
+    required: true,
+  },
+     
 });
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-module.exports = Blog;
+module.exports = Blog; 
+ 
